0d70d5498d7e5730becbb2b440a6a0ff3dca9865
Ankit-data-analyst <ankitpandawork@gmail.com>
2026-07-01 12:01:25 +0530

    Fixed a bug in contacts.js by entering a valid field for checking input

--- contacts.js
+++ contacts.js
@@ -6,7 +6,9 @@ const displayMessage = document.getElementById('displayMessage')
 button.addEventListener('click', () => {
     const nameValue = nameInput.value;
     const numberValue = numberInput.value;
-    if(numberValue.length==10)
+    if(numberValue.length==0 || nameInput.value==0)
+        displayMessage.innerHTML = 'Please provide an input'
+    else if(numberValue.length==10)
         displayMessage.innerHTML = 'Valid Phone Number'
     else
         displayMessage.innerHTML = 'Invalid Phone Number'
